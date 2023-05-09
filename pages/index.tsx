a4540fa2f5429927036db207e13fc5fb6ddac1cd
Mykhailo Nosov <mykhailo.nosov@visarsoft.de>
2023-05-09 13:06:49 +0300

    Moved header to separated component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,15 @@ import TargetComponent from "@/components/target/TargetComponent";
 import styles from "./index.module.css";
 import { values, targets } from "../tempData";
 import { useEffect, useState } from "react";
-import { getFormattedString, getNumberFromString } from "utils/formatting";
+import {
+    getFormattedString,
+    getMonthName,
+    getNumberFromString,
+} from "utils/formatting";
 import OrdersListComponent from "@/components/orders/OrdersListComponent";
 import { Order } from "utils/types";
 import { Product } from "utils/interfaces";
-import CountdownComponent from "@/components/countdown/CountdownComponent";
+import HeaderComponent from "@/components/header/HeaderComponent";
 
 const ordersCount = 5;
 
@@ -17,10 +21,6 @@ const Home = () => {
     const [ordersSum, setOrdersSum] = useState(0);
     const [filteredOrders, setFilteredOrders] = useState<Order[]>([]);
     const [popularProducts, setPopularProducts] = useState<Product[]>([]);
-
-    const getMonthName = (date: Date) => {
-        return date.toLocaleDateString("en-US", { month: "long" });
-    };
 
     const monthTarget = getNumberFromString(
         targets.find((el) => el.includes(getMonthName(currentDate)))?.[1]
@@ -141,56 +141,11 @@ const Home = () => {
             </div>
 
             <div className={styles.container}>
-                <div className={styles.head}>
-                    <div className="left">
-                        <p>Order Dashboard</p>
-
-                        <div className={styles.switcher}>
-                            <h2>
-                                {getMonthName(currentDate)}{" "}
-                                {currentDate.getFullYear()}
-                            </h2>
-
-                            <div className={styles.switcherBtns}>
-                                <button
-                                    onClick={() => changeCurrentMonth("next")}
-                                >
-                                    {/* prettier-ignore */}
-                                    <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<g clipPath="url(#clip0_3_161)">
-										<path d="M21.4638 10.1887L19.525 8.25L11.275 16.5L19.525 24.75L21.4638 22.8113L15.1663 16.5L21.4638 10.1887Z" fill="#C4C4C4"/>
-									</g>
-									<defs>
-										<clipPath id="clip0_3_161">
-											<rect width="33" height="33" fill="white"/>
-										</clipPath>
-									</defs>
-								</svg>
-                                </button>
-
-                                <button
-                                    onClick={() => changeCurrentMonth("prev")}
-                                >
-                                    {/* prettier-ignore */}
-                                    <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<g clipPath="url(#clip0_3_166)">
-										<path d="M11.5362 22.8113L13.475 24.75L21.725 16.5L13.475 8.25L11.5362 10.1887L17.8337 16.5L11.5362 22.8113Z" fill="#C4C4C4"/>
-									</g>
-									<defs>
-										<clipPath id="clip0_3_166">
-											<rect width="33" height="33" fill="white" transform="matrix(-1 0 0 -1 33 33)"/>
-										</clipPath>
-									</defs>
-								</svg>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="right">
-                        <CountdownComponent getData={getDataHandler} />
-                    </div>
-                </div>
+                <HeaderComponent
+                    changeCurrentMonth={(dir) => changeCurrentMonth(dir)}
+                    currentDate={currentDate}
+                    getDataHandler={getDataHandler}
+                />
 
                 {isLoading ? (
                     <div className={styles.loadingWrap}>
