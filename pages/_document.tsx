b18d05f1fd31a819124c7d9077d3d82de7c685ac
Mykhailo Nosov <mykhailo.nosov@visarsoft.de>
2023-05-09 17:39:08 +0300

    Fixed sorting, added fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="anonymous"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:wght@700&family=Source+Serif+Pro:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
