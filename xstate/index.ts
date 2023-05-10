bc10b22fe72ce2e4887acf4ee927cfe1dc338ab7
Mykhailo Nosov <mykhailo.nosov@visarsoft.de>
2023-05-10 16:58:09 +0300

    Added XState and moved data there

---
+++ xstate/index.ts
@@ -0,0 +1,76 @@
+import { Order, Target } from "utils/interfaces";
+import { getOrders, getTargets } from "../api/sheets";
+import { createMachine } from "xstate";
+
+const homeMachine = createMachine(
+    {
+        id: "home",
+        initial: "idle",
+        context: {
+            values: [] as Order[],
+            targets: [] as Target[],
+            error: null,
+        },
+        states: {
+            idle: {
+                on: {
+                    FETCH: "loading",
+                },
+            },
+            loading: {
+                invoke: {
+                    src: "fetchOrders",
+                    onError: {
+                        target: "failure",
+                        actions: "setError",
+                    },
+                    onDone: {
+                        target: "success",
+                        actions: "setOrders",
+                    },
+                },
+            },
+            success: {
+                on: {
+                    FETCH: "loading",
+                },
+            },
+            failure: {
+                on: {
+                    FETCH: "loading",
+                },
+            },
+        },
+    },
+    {
+        actions: {
+            setError: (context, event) => {
+                const { data: error } = event;
+
+                context.error = error.message;
+            },
+            setOrders: (context, event) => {
+                const {
+                    data: { values, targets },
+                } = event;
+
+                context.values = values;
+                context.targets = targets;
+            },
+        },
+        services: {
+            fetchOrders: async () => {
+                const values = getOrders();
+                const targets = getTargets();
+
+                return Promise.all([values, targets]).then(
+                    ([values, targets]) => {
+                        return { values, targets };
+                    }
+                );
+            },
+        },
+    }
+);
+
+export default homeMachine;
